c6e45658e3b2e407e98deb574a85f4f7ebb43e1d
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-24 14:24:48 -0600

    Arreglando errores

--- main.py
+++ main.py
@@ -7,20 +7,19 @@ from google.appengine.api import users
 template_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.getcwd()))
 
-
 class RequestPage(webapp2.RequestHandler):
-	def get(self):
+    def get(self):
         user = users.get_current_user()
 		login_url = users.create_login_url(self.request.path)
         logout_url = users.create_logout_url(self.request.path)
 
         template = template_env.get_template('home.html')
-        context ={
+        context = {
             'user': user,
             'login_url': login_url,
             'logout_url': logout_url,
         }
-        self.response.out.write(template.rende(context))
+        self.response.out.write(template.render(context))
 
-application = webapp2.WSGIApplication([('/',RequestPage)
-	],debug=True)
+application = webapp2.WSGIApplication([('/',RequestPage)],
+                                        debug=True)
