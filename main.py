2bf1280a2368a63d43b2b1e541f7700bf6bc9b31
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-24 14:19:00 -0600

    Agregando html e intentando hacer que corra

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 import webapp2
+import os
+import jinja2
+
+from google.appengine.api import users
+
+template_env = jinja2.Environment(
+    loader = jinja2.FileSystemLoader(os.getcwd()))
+
 
 class RequestPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.out.write('Hola dundo')
+        user = users.get_current_user()
+		login_url = users.create_login_url(self.request.path)
+        logout_url = users.create_logout_url(self.request.path)
+
+        template = template_env.get_template('home.html')
+        context ={
+            'user': user,
+            'login_url': login_url,
+            'logout_url': logout_url,
+        }
+        self.response.out.write(template.rende(context))
 
 application = webapp2.WSGIApplication([('/',RequestPage)
 	],debug=True)
