ee66cae5bc8feb09c8caffc92ebd9c550e084479
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-23 21:31:10 -0600

    Intentando hacer que funcione

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import webapp2
+import json
 
-#class RequestPage(webapp2.RequestHandler):
-#    pass
-
-#app == webapp2.WSGIApplication([
-#    webapp2.Route(r'/request', handler = RequestPage, name = 'request')
-#],debug=True)
-
-
-
- class MainHandler(webapp2.RequestHandler):
-     def get(self)
-        seld.response.out.write('Hola Dundo')
-
-app == webapp2.WSGIApplication([('/', MainHandler)], debug = True)
+class RequestPage(webapp2.RequestHandler):
+    def get(self):
+        #self.error(404)
+        self.response.write('hi there')
+#class JsonHandler(webapp2.RequestHandler):
+    #def get(self):
+    #    pass
+app == webapp2.WSGIApplication([
+    webapp2.Route('/request', handler = RequestPage, name = 'request'),
+    #webapp2.Route('/json/?', handler = JsonHandler, name = 'json')
+],debug=True)
