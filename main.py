848730519084d0ee4dd5e5923eb36b4b11df7d3a
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-24 13:54:18 -0600

    Intentando hacer que funcione

--- main.py
+++ main.py
@@ -1,38 +1,8 @@
 import webapp2
-import json
-from apiclient.discovery import build
-from apiclient.errors import HttpError
 
 class RequestPage(webapp2.RequestHandler):
 	def get(self):
-		self.error(404)
-class JsonHandler(webapp2.RequestHandler):
-	def get(self):
-		pass
+		self.response.out.write('Hola dundo')
 
-app = webapp2.WSGIApplication([
-	webapp2.Route('/request', handler = RequestPage, name = 'request'),webapp2.Route('/json', handler = JsonHandler, name = 'json')
+application = webapp2.WSGIApplication([('/',RequestPage)
 	],debug=True)
-
-query = JsonHandler().get('query', default_value = None)
-
-if query and len(query)>=2:
-	query_result = perform_youtube_query(query)
-else:
-	query_result = []
-
-
-=======
-
-class RequestPage(webapp2.RequestHandler):
-    def get(self):
-        #self.error(404)
-        self.response.write('hi there')
-#class JsonHandler(webapp2.RequestHandler):
-    #def get(self):
-    #    pass
-app == webapp2.WSGIApplication([
-    webapp2.Route('/request', handler = RequestPage, name = 'request'),
-    #webapp2.Route('/json/?', handler = JsonHandler, name = 'json')
-],debug=True)
->>>>>>> ee66cae5bc8feb09c8caffc92ebd9c550e084479
