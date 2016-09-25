52ac10e96d4a75b676fdecd3bf63dee273011889
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-25 15:55:52 -0600

    Intentando hacer que printee en la pagina web

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ CHANNEL_ID = "UCvS6-K6Ydmb4gH-kim3AmjA"
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
-    pass
+    search_response = urllib.urlopen("https://www.googleapis.com/"+YOUTUBE_API_SERVICE_NAME+"/"+YOUTUBE_API_VERSION+"/search?key="+DEVELOPER_KEY+"&channelId="+CHANNEL_ID+"&part=snippet,id&order=date&maxResults=20")
+    data = {}
+    data['response']= search_response.read()
+    json_data = json.dumps(data)
+    self.response("Hi there")
+    print(json_data)
+    
   def youtube_search(self,title,description):
     search_response = urllib.urlopen("https://www.googleapis.com/"+YOUTUBE_API_SERVICE_NAME+"/"+YOUTUBE_API_VERSION+"/search?key="+DEVELOPER_KEY+"&channelId="+CHANNEL_ID+"&part=snippet,id&order=date&maxResults=20")
     data = {}
@@ -21,5 +27,4 @@ class MainHandler(webapp2.RequestHandler):
 application = webapp2.WSGIApplication([('/',MainHandler),
                               ],debug = True)
 
-main = MainHandler()
-main.youtube_search("Robotics","")
+
