4e150e81d43fc60321748b9358db06f56f9c6012
Areyes-Trimarchi <alvaro.reyes@unitec.edu>
2016-09-25 15:20:35 -0600

    Probando codigo

--- main.py
+++ main.py
@@ -1,51 +1,23 @@
 import webapp2
 import json
-from googleapiclient import discovery
-from googleapiclient import errors
-from oauth2client.tools import argparser
+import urllib
 
-DEVELOPER_KEY = "AIzaSyBmjjEjlr2FfvxyTX4OR6Ljgk_WkvWTcPw"
+DEVELOPER_KEY = "AIzaSyAFM-ucrRbBBI8HYaf5Lk08tYLpjaBya34"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+CHANNEL_ID = "UCvS6-K6Ydmb4gH-kim3AmjA"
 
 class MainHandler(webapp2.RequestHandler):
-  def youtube_search(options):
-    youtube = discovery.build("youtube","v3",developerKey="AIzaSyBmjjEjlr2FfvxyTX4OR6Ljgk_WkvWTcPw")
+  def get(self):
+    pass
+  def youtube_search(self,title,description):
+    search_response = urllib.urlopen("https://www.googleapis.com/"+YOUTUBE_API_SERVICE_NAME+"/"+YOUTUBE_API_VERSION+"/search?key="+DEVELOPER_KEY+"&title="+title+"&description="+description+"&channelId="+CHANNEL_ID+"&part=snippet,id&order=date&maxResults=20")
+    data = {}
+    data['response']= search_response.read()
+    json_data = json.dumps(data)
+    print(json_data)
 
-    search_response = youtube.search().list(
-      q = "UCvS6-K6Ydmb4gH-kim3AmjA",
-      part = "id,snippet",
-      maxResults = 50,
-    ).execute()
-
-    videos = []
-    channels = []
-    playlists = []
-
-    for search_result in search_response.get("items", []):
-      if search_result["id"]["kind"] == "youtube#video":
-        videos.append("%s (%s)" % (search_result["snippet"]["title"],
-                                  search_result["id"]["videoId"]))
-      if search_result["id"]["kind"] == "youtube#channel":
-        channels.append("%s (%s)" % (search_result["snippet"]["title"],
-                                  search_result["id"]["channelId"]))
-      if search_result["id"]["kind"] == "youtube#playlist":
-        playlists.append("%s (%s)" % (search_result["snippet"]["title"],
-                                  search_result["id"]["playlistId"]))
-
-    print "Videos:\n", "\n".join(videos), "\n"
-    print "Channels:\n", "\n".join(channels), "\n"
-    print "Playlists:\n", "\n".join(playlists), "\n"
-
-  if __name__ == "__main__":
-    argparser.add_argument("--q", help="Search term", default="Google")
-    argparser.add_argument("--max-results", help="Max results", default=25)
-    args = argparser.parse_args()
-
-    try:
-      youtube_search(args)
-    except error.HttpError, e:
-      print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
-
+main = MainHandler()
+main.youtube_search("Robotics","")
 app = webapp2.WSGIApplication([('/',MainHandler),
                               ],debug = True)
